4843f2174bd946ee9ad46a93805bf978ab11b4ea
Gabriel Camargo <gcamargo.dev@gmail.com>
2024-05-05 20:09:32 -0300

    feat: create signin route method on authcontroller

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -2,10 +2,17 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignupDto } from './dto/signup.dto';
 import { Public } from 'src/shared/decorators/public.decorator';
+import { SigninDto } from './dto/signin.dto';
 
 @Controller('auth')
 export class AuthController {
 	constructor(private readonly authService: AuthService) { }
+
+	@Public()
+	@Post('signin')
+	signin(@Body() signinDto: SigninDto) {
+		return this.authService.signin(signinDto);
+	}
 
 	@Public()
 	@Post('signup')
