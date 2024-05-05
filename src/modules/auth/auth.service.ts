063f8f4427a9556ee7af3479542bbf25e77cdb24
Gabriel Camargo <gcamargo.dev@gmail.com>
2024-05-05 20:09:24 -0300

    feat: create signin service method on authservice

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,11 +1,12 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from '../user/entities/user.entity';
 import { Repository } from 'typeorm';
 import { SignupDto } from './dto/signup.dto';
 
-import { hash } from 'bcryptjs';
+import { hash, compare } from 'bcryptjs';
+import { SigninDto } from './dto/signin.dto';
 
 @Injectable()
 export class AuthService {
@@ -15,15 +16,34 @@ export class AuthService {
 		private readonly userRepo: Repository<User>
 	) { }
 
+	async signin({ username, password }: SigninDto) {
+		const user = await this.userRepo.findOne({
+			where: { username }
+		});
+
+		if (!user) {
+			throw new NotFoundException('User not found');
+		}
+
+		const isValidPassword = await compare(password, user.password);
+
+		if (!isValidPassword) {
+			throw new BadRequestException('Invalid credentials');
+		}
+
+		const accessToken = await this.generateAccessToken(user.id);
+		delete user.password;
+		return {
+			...user,
+			accessToken,
+		};
+	}
+
 	async signup({ email, password, username }: SignupDto) {
 		const userExists = await this.userRepo.findOne({
 			where: [
-				{
-					email
-				},
-				{
-					username
-				}
+				{ email },
+				{ username }
 			]
 		});
 
@@ -38,5 +58,12 @@ export class AuthService {
 			username,
 			password: hashedPassword
 		});
+	}
+
+	private generateAccessToken(userId: string) {
+		return this.jwtService.signAsync(
+			{ sub: userId },
+			{ secret: process.env.JWT_SECRET },
+		);
 	}
 }
