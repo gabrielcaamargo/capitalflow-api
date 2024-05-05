ab348f39d2dbd6eefd291cd8ff9b76c7815490f2
Gabriel Camargo <gcamargo.dev@gmail.com>
2024-05-05 19:52:08 -0300

    feat: create public route decorator and auth guard

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,11 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignupDto } from './dto/signup.dto';
+import { Public } from 'src/shared/decorators/public.decorator';
 
 @Controller('auth')
 export class AuthController {
 	constructor(private readonly authService: AuthService) { }
 
+	@Public()
 	@Post('signup')
 	signup(@Body() signupDto: SignupDto) {
 		return this.authService.signup(signupDto);
